f349b5bdbf2298b3ec3989ce5afcb7a3c6d94633
Jannis Pohlmann <jannis@thegraph.com>
2019-09-20 18:57:26 +0200

    ipfs: Only set IPFS port explicitly if not provided

--- src/ipfs.js
+++ src/ipfs.js
@@ -11,11 +11,20 @@ const createIpfsClient = node => {
 The URL must be of the following format: http(s)://host[:port]/[path]`)
   }
 
+  // Set the port to 443 or 80 explicitly, if no port was provided
+  if (!url.port) {
+    if (url.protocol === 'https:') {
+      url.port = 443
+    } else if (url.protocol === 'http:') {
+      url.port = 80
+    }
+  }
+
   // Connect to the IPFS node (if a node address was provided)
   return ipfsHttpClient({
     protocol: url.protocol.replace(/[:]+$/, ''),
     host: url.hostname,
-    port: url.protocol === 'https:' ? 443 : url.protocol === 'http' ? 80 : url.port,
+    port: url.port,
     'api-path': url.pathname.replace(/\/$/, '') + '/api/v0/',
   })
 }
